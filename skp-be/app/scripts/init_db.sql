3bb6f4325f41be03d09338c35a8994480b824a2d
ni-tami <n.c.utami@gmail.com>
2026-07-16 17:41:30 +0700

    combine sql scripts

--- skp-be/app/scripts/init_db.sql
+++ skp-be/app/scripts/init_db.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS users (
     expo_push_token TEXT,
     status TEXT DEFAULT 'available' CHECK (status IN ('available','busy')),
 );
+
+CREATE TABLE IF NOT EXISTS locations (
+    id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    latitude INTEGER NOT NULL,
+    longitude INTEGER,
+    PRIMARY KEY (id) FOREIGN KEY (user_id) REFERENCES users (id)
+);
