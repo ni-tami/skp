01be8dba5393f642bab20a6f7f2d9dbdc227a22e
tsanaativa <tsanaativa.vinnera@gmail.com>
2026-07-16 16:14:42 +0700

    Implement home screen

---
+++ skp-fe/src/app/(features)/routine/index.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+
+export default function RoutineScreen() {
+  return (
+    <View className="flex-row flex-wrap justify-between p-4">
+    </View>
+  );
+}
---
+++ skp-fe/src/components/home/HomeButton.tsx
@@ -0,0 +1,28 @@
+import { Ionicons } from "@react-native-vector-icons/ionicons/static";
+import { router } from "expo-router";
+import { ComponentProps } from "react";
+import { Pressable, Text } from "react-native";
+
+type IoniconsName = ComponentProps<typeof Ionicons>["name"];
+export default function HomeButton ({
+    title,
+    icon,
+    route,
+  }: {
+    title: string;
+    icon: IoniconsName;
+    route?: string;
+  }) {
+    return (
+    <Pressable
+      className='h-36 flex-1 items-center justify-center rounded-2xl shadow-slate-400 color-white'
+      onPress={() => {
+        if (route) {
+          router.push(route as any);
+        }
+      }}
+    >
+      <Ionicons name={icon} size={28} color="black" />
+      <Text className='text-lg font-semibold text-black'>{title}</Text>
+    </Pressable>
+    )};
